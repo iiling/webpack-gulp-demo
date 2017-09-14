dba9b7ce8d84aeb2e8ef5c6fbad365b3dd51bbfa
iiling <136970135@qq.com>
2017-09-14 18:24:44 +0800

    -mwebpack gulp

---
+++ dest/js/6a293fd6.bb.js
@@ -0,0 +1,31 @@
+webpackJsonp([1],{
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(9);
+
+
+/***/ }),
+
+/***/ 9:
+/***/ (function(module, exports, __webpack_require__) {
+
+	__webpack_require__(2);
+	__webpack_require__(10);
+
+	var Comp1 = __webpack_require__(8);
+
+	console.log('我是页面B, 我开始调用了组件Comp1了');
+	Comp1();
+
+/***/ }),
+
+/***/ 10:
+/***/ (function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ })
+
+});
